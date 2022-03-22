b1e8d0f5b00165056769cc11dc21a8ce4002f945
Saurabh <saurabh.d2106@gmail.com>
2022-03-22 13:31:47 +0530

    Add project files.

---
+++ LearningSpecflowWithSelenium/Features/ConduitApplication.feature
@@ -0,0 +1,6 @@
+﻿Feature: Post Articles
+	
+	Scenario: Add an article
+		Given As a writer
+		When I post an article
+		Then posted article should be visible for my learners
---
+++ LearningSpecflowWithSelenium/StepDefinitions/ConduitApplicationSteps.cs
@@ -0,0 +1,30 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace LearningSpecflowWithSelenium.StepDefinitions
+{
+    [Binding]
+    public sealed class ConduitApplicationSteps
+    {
+       
+
+        [Given(@"As a writer")]
+        public void GivenAsAWriter()
+        {
+
+        
+        }
+
+        [When(@"I post an article")]
+        public void WhenIPostAnArticle()
+        {
+           
+        }
+
+        [Then(@"posted article should be visible for my learners")]
+        public void ThenPostedArticleShouldBeVisibleForMyLearners()
+        {
+            
+        }
+    }
+}
